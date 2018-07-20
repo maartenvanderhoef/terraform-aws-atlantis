fffc8efd1a26c0b9074926a232773242791ffba5
maartenvanderhoef <maarten@blinkist.com>
2018-07-20 12:37:40 +0200

    minus gh outputs

--- outputs.tf
+++ outputs.tf
@@ -2,13 +2,3 @@ output "atlantis_url" {
   description = "URL of Atlantis"
   value       = "${local.atlantis_url}"
 }
-
-output "github_webhook_secret" {
-  description = "Github webhook secret"
-  value       = "${module.github_repository_webhook.this_repository_webhook_secret}"
-}
-
-output "github_webhook_urls" {
-  description = "Github webhook URL"
-  value       = "${module.github_repository_webhook.this_repository_webhook_urls}"
-}
